756a0a7ff092d01658858f17eb8e00348bd53915
cardrocha <cardrocharj@gmail.com>
2025-03-27 13:02:08 -0300

    style: remove redundant background color from App component and set body background

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { PlansBox } from "./components/plansBox";
 export function App() {
 
   return (
-    <main className="bg-black px-10 py-10 font-inria">
+    <main className="px-10 py-10 font-inria">
       <PlansBox />
     </main>
   )
