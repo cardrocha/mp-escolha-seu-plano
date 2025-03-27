da55a803da81edcd9509dde42009f62aed41144b
cardrocha <cardrocharj@gmail.com>
2025-03-27 11:34:38 -0300

    Refactor PlansBox component to simplify plan selection logic and improve button handling

--- src/components/plansBox.tsx
+++ src/components/plansBox.tsx
@@ -5,17 +5,13 @@ import { useState } from "react";
 export function PlansBox() {
   const [selectedPlan, setSelectedPlan] = useState(false);
 
-  const handleSelectPlan = () => {
-    setSelectedPlan(!selectedPlan);
-  }
-
   return (
     <>
-      <div className="flex justify-center pb-7">
-        <button onClick={handleSelectPlan} className={`border-2 border-white rounded-lg py-1 px-6 text-sm text-white transition-colors ${selectedPlan ? "bg-transparent hover:bg-gray-800" : "bg-gray-700 z-10"}`}>Mensal</button>
-        <button onClick={handleSelectPlan} className={`${selectedPlan ? "bg-gray-700" : "bg-transparent hover:bg-gray-800"} text-sm font-light text-amber-400 border-2 border-white rounded-lg py-1 px-6 -ml-2 transition-colors`}>Anual (20% de desconto)</button>
+      <div className="flex justify-center">
+        <button onClick={() => setSelectedPlan(false)} className={`border-2 border-white rounded-lg py-1 px-6 text-sm text-white transition-colors ${selectedPlan ? "bg-transparent hover:bg-gray-800" : "bg-gray-700 z-10"}`}>Mensal</button>
+        <button onClick={() => setSelectedPlan(true)} className={`${selectedPlan ? "bg-gray-700" : "bg-transparent hover:bg-gray-800"} text-sm font-light text-amber-400 border-2 border-white rounded-lg py-1 px-6 -ml-2 transition-colors`}>Anual (20% de desconto)</button>
       </div>
-      <div className="flex gap-7">
+      <div className="flex gap-7 mt-7">
         {plansData.map((item) => (
           <div key={item.id} className="inline-flex flex-col bg-black border-2 border-gray-500 p-5 rounded-2xl">
             <div className="flex gap-3 flex-col bg-background border-2 border-gray-500 py-12 px-10 w-[328px] rounded-md h-full">
